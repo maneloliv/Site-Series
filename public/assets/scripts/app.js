e3712882b3ba901bf2597b6571396e1141185f91
maneloliv <emanuelteiliveira@gmail.com>
2024-11-28 20:24:46 -0300

    modificação json

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -4,6 +4,7 @@ const ApiFavoritos = '/favoritos'
 const ApiProfile = '/profile'
 
 var db_fav= [];
+ 
 
 function ExibeSerie(){
     fetch (`https://api.themoviedb.org/3/discover/tv?api_key=${ApiKey}&language=pt-BR&`)
@@ -112,6 +113,7 @@ function ExibeDetalhes() {
   fetch(url, options)
     .then(res => res.json())
     .then(data => {
+        if(data.backdrop_path != null){
           let str = `
           <img src="https://image.tmdb.org/t/p/w500${data.backdrop_path}" class="img-fluid w-100" alt="Imagem da Série">
           <div class="overlay position-absolute top-0 start-0 w-100 h-100">
@@ -125,6 +127,22 @@ function ExibeDetalhes() {
           </div>`;
 
         document.getElementById('ExibeDetalhes').innerHTML = str;
+        }else{
+          let str = `
+          <img src="./assets/img/SerieExemplo.png" class="img-fluid w-100" alt="Imagem da Série">
+          <div class="overlay position-absolute top-0 start-0 w-100 h-100">
+            <div class="text-overlay text-white p-4">
+              <h1 class="display-4">${data.name}</h1>
+              <p class="lead fs-6">${data.overview}</p>
+              <div class="container-fluid text-end">
+                <button type="button" onclick="favoritar();SeriesFav()" id="likeButton" class="like-btn"><i class="bi bi-heart"></i></button>
+              </div>
+            </div>
+          </div>`;
+
+        document.getElementById('ExibeDetalhes').innerHTML = str;
+        }
+          
     })
 }
 
@@ -176,15 +194,41 @@ function BuscaSerie(event) {
   function favoritar() {
     const URLPage = new URLSearchParams(window.location.search);
     const ID = URLPage.get("id");
-    let fav = {"id": ID};
-    fetch(ApiFavoritos, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(fav),
-    });
+    fetch(ApiProfile)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erro ao buscar dados de usuários");
+        }
+        return response.json();
+    })
+    .then(data => {
+        let profileid = data[0].id;
+        fetch(ApiFavoritos)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Erro ao buscar dados de Favoritos");
+          }
+          return response.json();
+        })
+        .then(db_fav => {
+          let ultmoid = 0; 
+          for(let i;  i<db_fav.length; i++){
+            if(db_fav.id>= ultmoid){
+              ultmoid = db_fav.id;
+            }
+          }
+          let fav = {"id": ultmoid, "idprofile": profileid, "idtmdb": ID};
+          fetch(ApiFavoritos, {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(fav),
+          });
+        })
+    })
   }
 
   function CarregaProfile() {
+    
     fetch(ApiProfile)
         .then(response => {
             if (!response.ok) {
@@ -194,7 +238,11 @@ function BuscaSerie(event) {
         })
         .then(data => {
             console.log(data);
+            
             const usuario = data[0];
+            
+            
+            
             let str = '';
             str +=`
             <h1>Informação do Aluno</h1>
@@ -262,11 +310,11 @@ function SeriesFav(){
     
     for (let j = 0; j < db_fav.length; j++) {
      let fav = db_fav[j];
-     
-      console.log(fav.id);
+    
+      console.log(fav.idtmdb);
 
 
-      const url = 'https://api.themoviedb.org/3/tv/'+fav.id+'?language=en-US';
+      const url = 'https://api.themoviedb.org/3/tv/'+fav.idtmdb+'?language=en-US';
       const options = {
       method: 'GET',
       headers: {
@@ -280,7 +328,7 @@ function SeriesFav(){
           .then(data => {
             console.log(str);
                   str += `
-                    <a href="serie.html?id=${data.id}" target="_blank" class="col-2 my-2">
+                    <a href="serie.html?id=${data.idtmdb}" target="_blank" class="col-2 my-2">
                             <div class="card">
                                 <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="...">
                         </div></a>
